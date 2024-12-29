63bf643a4e45f02f3f492ee3b91bddaca9408982
Kevin <kevin.jourdain@etu.u-bordeaux.fr>
2024-12-29 19:40:32 +0100

    typo

--- src/challenge_125.rs
+++ src/challenge_125.rs
@@ -8,7 +8,7 @@ Given a string s, return true if it is a palindrome, or false otherwise.
 */
 
 pub fn is_palindrome(s: String) -> bool {
-    // this leetcode probably assumes ascii which is not very
+    // this leetcode probably assumes ascii which is not very smart
     // so we create an array of glyphs we can index into
     // this does allocation and copy but no way around it in the world of UTF-8
     let chars: Vec<char> = s
